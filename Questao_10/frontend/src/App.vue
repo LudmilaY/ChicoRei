a0d8ec52c51474cf58743fe5b9b05e5fe66c35df
Ludmila <lrbyung@gmail.com>
2025-03-17 22:02:48 -0300

    Adicionadas as pastas do front

---
+++ Questao_10/frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>Produtos</h1>
+    <div v-for="produto in produtos" :key="produto.id">
+      <h2>{{ produto.nome }}</h2>
+      <p>{{ produto.descricao }}</p>
+      <p>{{ produto.preco }}</p>
+      <img :src="'/storage/' + produto.imagem" alt="Imagem do produto" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      produtos: []
+    }
+  },
+  created() {
+    this.$http.get('http://localhost:8000/api/produtos').then(response => {
+      this.produtos = response.data;
+    });
+  }
+}
+</script>
+
+<style>
+
+</style>
